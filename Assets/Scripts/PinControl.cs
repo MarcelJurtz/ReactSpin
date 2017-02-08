25aa65372d8adcbb9e910f2637f2730098a27f4a
MarcelJurtz <jurtzmarcel@gmail.com>
2017-02-08 17:29:19 +0100

    Initial Commit

---
+++ Assets/Scripts/PinControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinControl : MonoBehaviour {
+
+    private bool isPinned = false;
+
+    public float speed = 20f;
+    public Rigidbody2D rBody;
+
+	void Update () {
+        if (!isPinned)
+        {
+            rBody.MovePosition(rBody.position + Vector2.up * speed * Time.deltaTime);
+        }
+	}
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.tag == "Rotator")
+        {
+            transform.SetParent(col.transform);
+
+            col.GetComponent<Rotator>().speed *= -1;
+
+            isPinned = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject PinPrefab;
+    public string button = "Fire1";
+
+    void Update()
+    {
+        if(Input.GetButtonDown(button))
+        {
+            SpawnPin();
+        }
+    }
+
+    void SpawnPin()
+    {
+        Instantiate(PinPrefab, transform.position, transform.rotation);
+    }
+}
